0705d0c0fb014d53ef5246a67f6cb3850ab05a5c
Eryk <erykokillo123@gmail.com>
2025-09-23 12:42:11 +0100

    Added keys to tasks

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   const [ taskState, setTaskState ] = useState({
     tasks:[
-      { title: "Dishes", description: "Empty dishwasher", deadline: "Today"},
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow"},
-      { title: "Tidy up" , deadline:"Today"}
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today"},
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow"},
+      { id: 3, title: "Tidy up" , deadline:"Today"}
     ]
   }
 
