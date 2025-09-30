bc461675fe7863c89271fa3281accbd9d633bbb9
Eryk <erykokillo123@gmail.com>
2025-09-30 11:51:16 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -21,6 +21,12 @@ function App() {
     console.log('${taskIndex} ${tasks[taskIndex].done}');
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  }
+
     return (
     <div className="container">
       <h1>Tasky</h1>
@@ -33,6 +39,7 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
     />
   ))} 
 
